73cd595d38a0db0fe412af0ff367cb149ddf857a
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-28 22:39:53 +0300

    Update cart

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const itemList = document.querySelector('#item-list')
+
+store.forEach((item)=>{
+    
+const imageContainer = document.createElement('div');
+imageContainer.setAttribute('class', 'col-3');
+
+const img = document.createElement('img');
+img.setAttribute('src', item.image)
+imageContainer.appendChild(img);
+
+const h5 =document.createElement('h5');
+h5.textContent = item.itemName
+
+const p =document.createElement('p');
+p.textContent = item.description
+
+const qty =document.createElement('input')
+
+qty.setAttribute('id',"");
+qty.setAttribute('placeholder','QTY');
+qty.setAttribute('type','number');
+
+const addToCartBtn = document.createElement('button')
+addToCartBtn.setAttribute('class', 'btn btn-warning btn-sm m-4')
+addToCartBtn.textContent = "Add To Cart";
+
+
+imageContainer.appendChild(h5)
+imageContainer.appendChild(p)
+imageContainer.appendChild(qty)
+imageContainer.appendChild(addToCartBtn)
+
+itemList.appendChild(imageContainer)
+
+})
